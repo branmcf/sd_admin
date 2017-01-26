f74ce51ea0d619ebb64d8ac604d5b6f7241ec7b6
DNguyen17 <danhn@smu.edu>
2017-01-25 23:16:08 -0600

    testing routing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  toLogIn() {
+    this.router.navigate(['entry/']);
   }
 
 }
