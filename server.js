4e9f2cfd3341ec12a5c3afa5d908bc247ae595f5
DNguyen17 <danhn@smu.edu>
2017-01-31 01:11:30 -0600

    pointing to elastic beanstalk

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+// app.use(forceSSL());
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
