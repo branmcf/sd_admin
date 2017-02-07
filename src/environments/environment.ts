5a9924c48cee71314c9e1f868334bd2e36d01300
DNguyen17 <danhn@smu.edu>
2017-02-07 02:45:50 -0600

    deploying

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,5 @@ export const environment = {
   production: false,
   CONTENTFUL_SPACE: '65c06hb0hter',
   CONTENTFUL_TOKEN: '7e4d86d07d34ee962ad350414402cb15349bb43f799e7d060288ab38006176e8',
-  API_URL: 'http://localhost:3000/'
+  API_URL: 'http://production-server.fpbmw68xg5.us-west-2.elasticbeanstalk.com/'
 };
