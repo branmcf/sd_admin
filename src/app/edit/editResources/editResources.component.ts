9d434c7092a0cb09bc33b5bf7cb649271799790f
Justin Suh <jsuh@smu.edu>
2017-04-01 19:14:16 -0500

    Update editResources.component.ts

--- src/app/edit/editResources/editResources.component.ts
+++ src/app/edit/editResources/editResources.component.ts
@@ -2,21 +2,19 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { SubmitService } from './../../services/submit.service';
 import { ReviewService } from './../../services/review.service';
-import { MdDialog, MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'hymn-edit-resources',
-  templateUrl: './editResources.html',
+  templateUrl: './editresources.html',
   styleUrls: ['./editResources.scss']
 })
 
 export class EditResourcesComponent implements OnInit {
-
   id: number;
   resource: any;
   catArr: any[];
   approved: boolean;
-  deleted: boolean;
+  eleted: boolean;
 
   constructor (private route: ActivatedRoute,
     private router: Router,
@@ -27,19 +25,30 @@ export class EditResourcesComponent implements OnInit {
     this.route.params.forEach(x => this.load(+x['id']));
   }
 
-    private load(id){
-        if(!id) {
+    private load(id) {
+        if (!id) {
             return;
         }
 
         var onload = (data) => {
-            if(data){
+            if (data) {
                 this.id = id;
-                data.categories = JSON.stringify(data);
                 this.resource = data;
             }
         };
         this.reviewService.getResourceByID(id).then(onload);
     }
-}
 
+
+    approve(id) {
+        this.reviewService.approveResource(this.id).then(() => {
+            this.router.navigate(['/review/resources']);
+        });
+    }
+
+    delete(id) {
+        this.reviewService.deleteResource(this.id).then(()=> {
+            this.router.navigate(['/review/resources']);
+        });
+    }
+}
