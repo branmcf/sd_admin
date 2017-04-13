bfe2681fe516544211926378b440ed9e48679100
DNguyen17 <danhn@smu.edu>
2017-04-13 02:19:02 -0500

    search functionality

--- src/app/view/viewResources/viewResources.component.ts
+++ src/app/view/viewResources/viewResources.component.ts
@@ -11,6 +11,7 @@ import * as _ from 'lodash';
 
 export class ViewResourcesComponent implements OnInit {
   resources: any[];
+  allResources: any[];
   type: string;
   icon: string;
   searchTerm: string;
@@ -25,20 +26,35 @@ export class ViewResourcesComponent implements OnInit {
     this.sub = this.route.params.subscribe(param => {
       this.type = param['type'];
       if (param['type'] === 'events') {
-        this.reviewService.getAllEvents('approved').then(x => this.resources = x).catch(err => console.log(err));
+        this.reviewService.getAllEvents('approved').then(x => {
+          this.resources = x;
+          this.allResources = x;
+        }).catch(err => console.log(err));
         this.icon = 'event';
       } else if (param['type'] === 'congregations') {
-        this.reviewService.getAllCongregations('approved').then(x => this.resources = x).catch(err => console.log(err));
+        this.reviewService.getAllCongregations('approved').then(x => {
+          this.resources = x;
+          this.allResources = x;
+        }).catch(err => console.log(err));
         this.icon = 'room';
       } else if (param['type'] === 'orgs') {
-        this.reviewService.getAllOrganizations('approved').then(x => this.resources = x).catch(err => console.log(err));
+        this.reviewService.getAllOrganizations('approved').then(x => {
+          this.resources = x;
+          this.allResources = x;
+        }).catch(err => console.log(err));
         this.icon = 'group_work';
         this.type = 'organization';
       } else if (param['type'] === 'persons') {
-        this.reviewService.getAllPersons('approved').then(x => this.resources = x).catch(err => console.log(err));
+        this.reviewService.getAllPersons('approved').then(x => {
+          this.resources = x;
+          this.allResources = x;
+        }).catch(err => console.log(err));
         this.icon = 'face';
       } else {
-        this.reviewService.getAllResources('approved/type/' + param['type']).then(x => this.resources = x).catch(err => console.log(err));
+        this.reviewService.getAllResources('approved/type/' + param['type']).then(x => {
+          this.resources = x;
+          this.allResources = x;
+        }).catch(err => console.log(err));
         this.icon = 'book';
       }
     });
@@ -49,8 +65,27 @@ export class ViewResourcesComponent implements OnInit {
   }
 
   search(term: string) {
-    const res = ['thinking out loud', 'this', 'is', 'abcde', 'omg', 'omg', 'this'];
-    console.log(res.indexOf(term));
+    if (this.type === 'persons') {
+      const res = _.filter(this.allResources, (x) => { return x.first_name.includes(term) ||
+                                                              x.last_name.includes(term); });
+      this.resources = res;
+    } else if (this.type === 'events') {
+      const res = _.filter(this.allResources, (x) => { return x.title.includes(term); });
+      this.resources = res;
+    } else if (this.type === 'congregations' || this.type === 'organization') {
+      const res = _.filter(this.allResources, (x) => { return x.name.includes(term); });
+      this.resources = res;
+    } else {
+      const res = _.filter(this.allResources, (x) => { return x.title.includes(term) ||
+                                                              x.author.includes(term) ||
+                                                              x.description.includes(term); });
+      this.resources = res;
+    }
+  }
+
+  clearSearch() {
+    this.resources = this.allResources;
+    this.searchTerm = '';
   }
 
   expand(resource) {
