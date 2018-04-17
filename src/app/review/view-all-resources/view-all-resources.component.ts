747143d51eb612d8de7af8042400034607263a72
Brandon McFarland <branmcf@gmail.com>
2018-04-17 15:26:46 -0500

    table work

--- src/app/review/view-all-resources/view-all-resources.component.ts
+++ src/app/review/view-all-resources/view-all-resources.component.ts
@@ -2,11 +2,8 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { SubmitService } from './../../services/submit.service';
 import { ReviewService } from './../../services/review.service';
-import { DataSource } from '@angular/cdk/collections';
-import { CdkTableModule } from '@angular/cdk/table';
-import { Observable } from 'rxjs/Rx';
-// import { MatTableModule } from '@angular/material';
-// import { MatTableDataSource } from '@angular/material';
+// import {MdTableModule} from '@angular/material';
+// import {CdkTableModule} from '@angular/cdk';
 
 
 @Component({
@@ -14,74 +11,25 @@ import { Observable } from 'rxjs/Rx';
   templateUrl: './view-all-resources.component.html'
 })
 export class ViewAllResourcesComponent implements OnInit {
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource: eDataSource;
-  ELEMENT_DATA: Element[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-    {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-    {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-    {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-    {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-    {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-    {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-    {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-    {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-    {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-    ];
+  resources: any[];
+  id: number;
+  arrLen: Number;
 
-  ngOnInit() {
-    this.dataSource = new eDataSource(this.ELEMENT_DATA);
-  }
-}
-
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-export class eDataSource extends DataSource<any> {
-
-  constructor(private element: Element[]) {
-    super();
+  constructor (private route: ActivatedRoute,
+    private router: Router,
+    private reviewService: ReviewService) {
+      this.reviewService.getAllResources()
+        .then(x => this.resources = x)
+        .then(x => this.arrLen = this.resources.length)
+        .then(x => console.log('ARE LEN IS: ', this.arrLen))
+        .then(x => console.log('RESOURCES ARE: ', this.resources))
+        .catch(err => {});
   }
 
-  connect(): Observable<Element[]> {
-    return Observable.of(this.element);
+  ngOnInit() {}
+
+  edit(id) {
+    this.router.navigate(['/entry/review/resources', id]);
   }
 
-  disconnect() {}
 }
-
-  // resources: any[];
-  // id: number;
-  // arrLen: Number;
-  
-  // constructor (private route: ActivatedRoute,
-  //   private router: Router,
-  //   private reviewService: ReviewService) {
-  //     this.reviewService.getAllResources()
-  //       .then(x => this.resources = x)
-  //       .then(x => this.arrLen = this.resources.length)
-  //       .then(x => console.log('ARE LEN IS: ', this.arrLen))
-  //       .then(x => console.log('RESOURCES ARE: ', this.resources))
-  //       .catch(err => {});
-  // }
-
-  // ngOnInit() {}
-
-  // edit(id) {
-  //   this.router.navigate(['/entry/review/resources', id]);
-  // }
-// }
