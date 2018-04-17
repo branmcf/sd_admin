493381b0073e086287a138b617b765bafe5608b7
Brandon McFarland <branmcf@gmail.com>
2018-04-17 14:21:17 -0500

    reinstall node modules

--- src/app/review/view-all-resources/view-all-resources.component.ts
+++ src/app/review/view-all-resources/view-all-resources.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { SubmitService } from './../../services/submit.service';
 import { ReviewService } from './../../services/review.service';
+import {MdTableModule} from '@angular/material';
+import {CdkTableModule} from '@angular/cdk';
+
 
 @Component({
   selector: 'app-view-all-resources',
