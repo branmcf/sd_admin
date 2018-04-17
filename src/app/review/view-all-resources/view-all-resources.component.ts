5c394e8c2c605d10d7d5a59cc305d1d204726917
Brandon McFarland <branmcf@gmail.com>
2018-04-17 13:40:20 -0500

    router work

--- src/app/review/view-all-resources/view-all-resources.component.ts
+++ src/app/review/view-all-resources/view-all-resources.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { SubmitService } from './../../services/submit.service';
+import { ReviewService } from './../../services/review.service';
 
 @Component({
   selector: 'app-view-all-resources',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: []
 })
 export class ViewAllResourcesComponent implements OnInit {
+  resources: any[];
+  id: number;
 
-  constructor() { }
+  constructor (private route: ActivatedRoute,
+    private router: Router,
+    private reviewService: ReviewService) {
+      this.reviewService.getAllResources()
+        .then(x => this.resources = x)
+        .then(x => console.log('RESOURCES ARE: ', this.resources))
+        .catch(err => {});
+  }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  edit(id) {
+    this.router.navigate(['/entry/review/resources', id]);
   }
 
 }
