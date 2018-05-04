720a90bc8292c448eababf8ed83fbfb95d3274ad
Logan Dorsey <logandorsey@gmail.com>
2018-05-04 01:09:56 -0500

    Forgot a backlash

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,5 +12,5 @@ export const environment = {
 
   // API_URL: 'http://localhost:3000/'
   // API_URL: 'https://private-91abd-node46.apiary-mock.com/'
-  API_URL: 'https://backend.ccshymndb.com'
+  API_URL: 'https://backend.ccshymndb.com/'
 };
